f63c3b6881cc843707a10274203f49b811ce3e26
joshua <54235339+sujoshua@users.noreply.github.com>
2026-02-17 11:42:18 +0800

    :sparkles: add oidc support (#25)
    Co-authored-by: D <845765@qq.com>

--- app/src/main/java/org/b3log/siyuan/Utils.java
+++ app/src/main/java/org/b3log/siyuan/Utils.java
@@ -38,6 +38,7 @@ import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import androidx.annotation.StringRes;
+import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
@@ -81,6 +82,9 @@ public final class Utils {
      * App version.
      */
     public static final String version = BuildConfig.VERSION_NAME;
+
+    private static volatile boolean authKeepAliveActive = false;
+    private static final String CUSTOM_TABS_KEEP_ALIVE = "android.support.customtabs.extra.KEEP_ALIVE";
 
     /**
      * App version code.
@@ -407,6 +411,22 @@ public final class Utils {
         final Uri uri = Uri.parse(url);
         final Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
         activity.startActivity(browserIntent);
+    }
+
+    // Try opening the URL with Custom Tabs; fall back to default browser on failure.
+    // Returns true if Custom Tabs was used, false if we fell back.
+    public static boolean tryOpenCustomTabs(Uri uri, final Activity activity) {
+        try {
+            final CustomTabsIntent customTabsIntent = new CustomTabsIntent.Builder()
+                    .setShowTitle(true)
+                    .build();
+
+            customTabsIntent.launchUrl(activity, uri);
+            return true;
+        } catch (final Exception e) {
+            openByDefaultBrowser(uri.toString(), activity);
+            return false;
+        }
     }
 
     public static String getLanguage() {
